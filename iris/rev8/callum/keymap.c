8471b0b9f70e363556d7195264c96f552200d5c6
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2024-07-01 20:11:07 +0200

    fixes

--- iris/rev8/callum/keymap.c
+++ iris/rev8/callum/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, 
         _______, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,                    KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, _______, 
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, 
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, 
                                             _______, _______, _______, _______, _______, _______
     )
 };
--- iris/rev8/callum/rules.mk
+++ iris/rev8/callum/rules.mk
@@ -3,6 +3,5 @@ LTO_ENABLE = yes
 ENCODER_MAP_ENABLE = no
 CONSOLE_ENABLE = no
 
-SRC += callum.c
 SRC += oneshot.c
 SRC += swapper.c
