465b064a4760c8e22e7e5081fc83f2357559d8f7
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2024-09-11 21:24:47 +0200

    tweak keymap

--- keyboards/handwired/dactyl_manuform/5x6_3/keymaps/colemak_v1/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_3/keymaps/colemak_v1/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEF] = LAYOUT(
         KC_ESC,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         KC_TAB,  KC_Q   , KC_W,    KC_F,    KC_P,    KC_B   ,                   KC_J   , KC_L,    KC_U   , KC_Y,    KC_SCLN, KC_DEL,
-        KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                   KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , TMUX,
+        KC_LALT, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                   KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , TMUX,
         KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                   KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, LA_I3,
                                             LA_NUM,  LA_NAV , KC_LSFT, KC_SPC , LA_SYM,  LA_FUN
     ),
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______, _______, KC_0,    _______
     ),
     [_FUN] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+        KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
         _______, _______, _______, KC_BSPC, KC_DEL,  _______,                   KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PERC, _______,
         _______, OS_CMD,  OS_ALT,  OS_SHFT, OS_CTRL, _______,                   KC_F11,  KC_F4,   KC_F5,   KC_F6,   _______, _______,
         _______, _______, _______, _______, _______, _______,                   KC_F10,  KC_F1,   KC_F2,   KC_F3,   _______, _______,
