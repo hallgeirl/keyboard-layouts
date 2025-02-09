10f30e1a0ba4830ada8978f9c3d9a7f1565a7449
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2025-02-09 17:38:51 +0100

    fixes

--- keyboards/handwired/dactyl_manuform/5x7_5/keymaps/colemak_v1/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_5/keymaps/colemak_v1/keymap.c
@@ -91,8 +91,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEF] = LAYOUT(
         KC_ESC,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_VOLU, KC_HOME, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , LA_GA1,
         KC_TAB,  KC_Q   , KC_W,    KC_F,    KC_P,    KC_B   , KC_VOLD, KC_PGUP, KC_J   , KC_L,    KC_U   , KC_Y,    KC_SCLN, KC_DEL,
-        KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_MPLY, KC_PGDN, KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , TMUX,
-        KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_MNXT, KC_END,  KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, LA_I3,
+        KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_MNXT, KC_PGDN, KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , TMUX,
+        KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_MPLY, KC_END,  KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, LA_I3,
                           NO_Æ,    NO_Å,    KC_LCTL, LA_NAV,  KC_LSFT, KC_SPC,  LA_SYM,  LA_FUN,  NO_Ø,    KC_MINS,
                                                      KC_BSPC, KC_DEL,  KC_LALT, KC_ENT
     ),
