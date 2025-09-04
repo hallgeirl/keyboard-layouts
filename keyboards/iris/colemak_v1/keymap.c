d4bb7d95227c720be19596ca880acb98426c6d1a
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2025-09-04 18:18:08 +0200

    swap master etc

--- keyboards/iris/colemak_v1/keymap.c
+++ keyboards/iris/colemak_v1/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q   , KC_W,    KC_F,    KC_P,    KC_B   ,                   KC_J   , KC_L,    KC_U   , KC_Y,    KC_SCLN, KC_DEL,
         KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                   KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , TMUX,
         KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , LA_GA1,  LA_SYS,  KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, LA_I3,
-                                            LA_NUM,  LA_NAV , KC_LSFT, KC_SPC , LA_SYM,  LA_FUN
+                                            KC_BSPC, LA_NAV , KC_SPC , KC_LSFT, LA_SYM,  LA_FUN
     ),
 	[_GA1] = LAYOUT(
         KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
